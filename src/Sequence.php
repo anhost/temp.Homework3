df3be7a5b8921965aefbf7670eecfadcc58f0446
DShevchenko <mail@anhost.ru>
2023-11-19 18:24:24 +0300

    Init

---
+++ src/Sequence.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dshevchenko\Mysterysequence;
+
+class Sequence {
+    public function get(): array {
+        return [4, 8, 15, 16, 23, 42];
+    }
+}
